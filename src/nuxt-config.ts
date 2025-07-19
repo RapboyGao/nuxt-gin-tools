efcbbf83e96060f7b77855e94fb9a8b66abff189
RapboyGao <atgao74201@qq.com>
2025-07-19 20:50:39 +0800

    2025.07.19 更新nuxt-config

--- src/nuxt-config.ts
+++ src/nuxt-config.ts
@@ -1,7 +1,7 @@
 export const config = {
   ssr: false,
-
-  buildDir: "vue/.nuxt",
+  buildDir: "vue/.nuxt", // 设置构建目录为 "vue/.nuxt"，表示 Nuxt 项目的构建输出将存放在该目录下
+  srcDir: "vue", // 设置源代码目录为 "vue"，表示 Nuxt 项目的源代码将存放在该目录下
   /**
    * 配置实验性功能
    * 禁用 payloadExtraction 功能，该功能可能用于提取页面的有效负载数据
@@ -10,4 +10,13 @@ export const config = {
   experimental: {
     payloadExtraction: false,
   },
+  // 配置 Nuxt DevTools
+  // 启用时间线功能，可能用于调试和性能分析
+  devtools: {
+    timeline: {
+      enabled: true,
+    },
+  },
 };
+
+export default config;
