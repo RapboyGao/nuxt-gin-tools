024fee39ed2f184ddd90619c2efbad3afe59a2be
RapboyGao <atgao74201@qq.com>
2025-07-19 10:07:14 +0800

    2025.07.19

--- src/nuxt-config.ts
+++ src/nuxt-config.ts
@@ -1,7 +1,6 @@
 import { readJSONSync } from "fs-extra";
 import { resolve } from "path";
-// @ts-ignore
-import { BASE_PATH } from "../../../vue/composables/api/api";
+import type { NuxtConfig } from "nuxt/config";
 
 const cwd = process.cwd();
 const serverConfig = readJSONSync(resolve(cwd, `server.config.json`)) as ServerConfig;
@@ -16,31 +15,12 @@ export interface ServerConfig {
   baseUrl: string;
 }
 
-/**
- * 处理基础路径，去除协议和域名部分，只保留路径部分
- * 例如，将 "https://example.com/api-go" 转换为 "/api-go"
- *
- */
-const thisBasePath = BASE_PATH.replace(/^https?:[/]{2}[^/]+/, "");
-
-/**
- * 定义代理目标 URL
- * 拼接本地服务器的地址和处理后的基础路径，用于开发环境的代理
- * 例如，当 Gin 服务器端口为 8099 时，目标 URL 可能为 "http://localhost:8099/api-go"
- */
-const target = `http://localhost:${serverConfig.ginPort}${thisBasePath}`;
-
-export const config = {
+export const config: NuxtConfig = {
   ssr: false,
   app: { baseURL: serverConfig.baseUrl },
   devServer: { port: serverConfig.nuxtPort },
   experimental: { payloadExtraction: false },
 
-  nitro: {
-    devProxy: {
-      [thisBasePath]: { target, changeOrigin: true },
-    },
-  },
   vite: {
     esbuild: { jsxFactory: "h", jsxFragment: "Fragment" },
   },
@@ -53,4 +33,10 @@ export const config = {
   },
 };
 
-export default config;
+export function getServerConfig(nitro: NuxtConfig["nitro"]): NuxtConfig {
+  return {
+    ...config,
+    nitro,
+  };
+}
+export default getServerConfig;
