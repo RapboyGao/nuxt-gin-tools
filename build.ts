bd4314fe32a3cca04c588976943de62f009e314b
RapboyGao <atgao74201@qq.com>
2025-07-19 09:37:00 +0800

    2025.07.19 更新buildscript顺序

--- build.ts
+++ build.ts
@@ -2,6 +2,10 @@ import concurrently from "concurrently";
 import { copySync, removeSync } from "fs-extra";
 
 export async function build() {
+  copySync("package.json", "dist/package.json");
+  copySync("README.md", "dist/README.md");
+  copySync("LICENSE", "dist/LICENSE");
+  copySync("src/go-gin-server.json", "dist/src/go-gin-server.json");
   // 执行并发命令
   await concurrently([
     {
@@ -10,11 +14,6 @@ export async function build() {
       prefixColor: "green",
     },
   ]).result;
-
-  copySync("package.json", "dist/package.json");
-  copySync("README.md", "dist/README.md");
-  copySync("LICENSE", "dist/LICENSE");
-  copySync("src/go-gin-server.json", "dist/src/go-gin-server.json");
   removeSync("dist/build.js");
   removeSync("dist/build.d.ts");
 }
