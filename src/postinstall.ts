4b4476e88463b30a6ab9aa653b23b695e08826f1
RapboyGao <atgao74201@qq.com>
2026-01-29 18:27:08 +0800

    feat: 在 postinstall 中添加 air 工具的安装
    在 go 命令中增加安装 air 工具的步骤，用于开发时自动重载

--- src/postinstall.ts
+++ src/postinstall.ts
@@ -4,7 +4,8 @@ export function postInstall() {
   // 执行并发命令
   return concurrently([
     {
-      command: "go mod download && go mod tidy",
+      command:
+        "go mod download && go mod tidy && go install github.com/cosmtrek/air@latest",
       name: "go",
       prefixColor: "green",
     },
